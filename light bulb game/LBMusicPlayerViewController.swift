a8b5e6ff6d8e49193e43d080febe33e0ca0892d7
Guilherme Martins <martinsg88@gmail.com>
2014-09-17 12:20:55 +0900

    Updated it to contain logic for the soundboard system

--- light bulb game/LBMusicPlayerViewController.swift
+++ light bulb game/LBMusicPlayerViewController.swift
@@ -7,3 +7,61 @@
 //
 
 import Foundation
+import UIkit
+import AVFoundation
+
+class LBMusicPlayerViewController : UIViewController, AVAudioPlayerDelegate{
+    
+    var musicOnePlayer: AVAudioPlayer!
+    var musicTwoPlayer: AVAudioPlayer!
+    var effectPlayer: AVAudioPlayer!
+    
+    
+    func playMusicOneButtonTap(){
+        let path = NSBundle.mainBundle().pathForResource("music", ofType:"mp3");
+        let url = NSURL(fileURLWithPath: path!);
+        musicOnePlayer = AVAudioPlayer(contentsOfURL: url, error: nil);
+        musicOnePlayer.prepareToPlay();
+        musicOnePlayer.delegate = self
+        musicOnePlayer.play();
+
+    }
+    
+    func playMusicTwoButtonTap(){
+        let path = NSBundle.mainBundle().pathForResource("gun", ofType:"mp3");
+        let url = NSURL(fileURLWithPath: path!);
+        musicTwoPlayer = AVAudioPlayer(contentsOfURL: url, error: nil);
+        musicTwoPlayer.prepareToPlay();
+        musicTwoPlayer.delegate = self
+        musicTwoPlayer.play();
+    }
+    
+    func playEffectButtonTap(){
+        let path = NSBundle.mainBundle().pathForResource("snare", ofType:"mp3");
+        let url = NSURL(fileURLWithPath: path!);
+        effectPlayer = AVAudioPlayer(contentsOfURL: url, error: nil);
+        effectPlayer.prepareToPlay();
+        effectPlayer.delegate = self
+        effectPlayer.play();
+   
+    }
+    
+    @IBAction func playReturnButtonTap(sender: AnyObject){
+        self.navigationController?.popViewControllerAnimated(false)
+    }
+    
+    @IBAction func didEffectButtonTap(sender: AnyObject){
+        
+    }
+    
+    @IBAction func didMusicTwoButtonTap(sender: AnyObject){
+        
+    }
+    
+    @IBAction func didMusicOneButtonTap(sender: AnyObject){
+        
+    }
+    
+    
+}
+
